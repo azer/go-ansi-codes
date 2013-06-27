b7e1d2c8c8e2e63ad724cc0059467816081f1620
E.Azer Koçulu <azer@kodfabrik.com>
2013-06-27 00:19:14 -0700

    use test.go for testing

---
+++ test/test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/azer/ansi-codes.go"
+	"github.com/azer/test.go"
+)
+
+func main () {
+
+	test.It("exports ansi codes of styles", func (expect test.Expect) {
+		expect(ansicodes.Red, "\033[31m")
+		expect(ansicodes.Blue, "\033[34m")
+		expect(ansicodes.Bold, "\033[1m")
+		expect(ansicodes.YellowBg, "\033[43m")
+		expect(1, 2)
+		expect(3, 4)
+	})
+
+	test.It("faks", func (expect test.Expect) {
+		expect(5, 6)
+	})
+
+}
